a6b4c30dafd15c12e99375c2c2492c497e30cb93
Seva Mironovich <mironovich@live.ru>
2020-05-13 01:59:05 +0300

    update build

---
+++ build/precache-manifest.b8e51a617c054d4ef09a0d2cad55e61c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bc05ed38e322255a161e81164013465e",
+    "url": "/md5/index.html"
+  },
+  {
+    "revision": "6125794305a9335b5faf",
+    "url": "/md5/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "b866afb84271a5e2bbf4",
+    "url": "/md5/static/js/2.0d4742b7.chunk.js"
+  },
+  {
+    "revision": "7edc9d6a5411dd85ffa426b7d3cb6e20",
+    "url": "/md5/static/js/2.0d4742b7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6125794305a9335b5faf",
+    "url": "/md5/static/js/main.de83f44b.chunk.js"
+  },
+  {
+    "revision": "cd6bab376627b9276d75",
+    "url": "/md5/static/js/runtime-main.fb65e291.js"
+  }
+]);
